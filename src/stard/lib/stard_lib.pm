04b8282d9354abd43494650de11c6becc00f934a
Jeryia <Jeryia@users.noreply.github.com>
2015-10-11 06:23:06 -0600

    fixed legal stuff

--- src/stard/lib/stard_lib.pm
+++ src/stard/lib/stard_lib.pm
@@ -3,7 +3,7 @@ use strict;
 use Carp;
 use Config::IniFiles;
 
-#Copyright (c) 2015, Jeryia
+
 #All rights reserved.
 #
 #Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
