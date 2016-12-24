cf7ac1454a140b5afc31e757458f1f60ca32829d
Jeryia <Jeryia@users.noreply.github.com>
2016-12-24 10:30:20 -0700

    added remap factions, recenter map, and repair map capabilities to the map lib

--- src/stard/lib/perl/Starmade/Map.pm
+++ src/stard/lib/perl/Starmade/Map.pm
@@ -6,6 +6,7 @@ use Carp;
 
 use Starmade::Base;
 use Starmade::Sector;
+use Starmade::Map;
 use Stard::Log;
 
 
@@ -15,7 +16,7 @@ our (@ISA, @EXPORT);
 
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT= qw(starmade_setup_lib_env starmade_clean_map_area starmade_setup_map);
+@EXPORT= qw(starmade_setup_lib_env starmade_clean_map_area starmade_setup_map starmade_repair_map starmade_recenter_map starmade_remap_map_factions);
 
 
 
@@ -150,6 +151,88 @@ sub starmade_setup_map {
 	return 1;
 };
 
+## starmade_repair_map
+# Create whatever objects are needed by the given map if they do not exist.
+# INPUT1: map config (hash)
+# OUTPUT: (boolean) true if successfull false if not.
+sub starmade_repair_map {
+	my %map_config = %{$_[0]};
+
+	my %object_mappings = ();
+
+	Object: foreach my $object (keys %map_config) {
+		if ($object eq "General") {
+			next Object;
+		}
+		my $sector = $map_config{$object}{sector};
+		my $blueprint = $map_config{$object}{blueprint};
+		my $owner = $map_config{$object}{owner};
+		my $npc;
+
+		my %sector_info = starmade_sector_info($sector);
+	
+	
+		starmade_cmd("/load_sector_range $sector $sector");
+
+
+		if (!$sector_info{entity}{$object} || $sector_info{entity}{$object}{$owner}) {
+			stdout_log("Recreating object '$object' as it does not appear be to exist", 6);
+			# Get rid of anything that already has the name (as names need to be unique).
+			if (!starmade_despawn_all($object, "all", "0")) {
+				starmade_broadcast("Error despawning all.\n");
+				print starmade_last_output();
+				stdout_log("Error despawning all...", 1);
+				return 0;
+			}
+			starmade_spawn_entity($blueprint, $object, $sector, $owner, 0);
+		}
+
+
+		if ($map_config{$object}{defenders}) {
+			my @defenders = split(",", $map_config{$object}{defenders});
+			my @pos = (); 
+
+			foreach my $defender (@defenders) {
+				starmade_despawn_sector($defender, 'unused', 0, $sector);
+			}
+
+			if ($map_config{$object}{defender_pos}) {
+				@pos = split(",", $map_config{$object}{defender_pos});
+			};
+			starmade_spawn_mobs_bulk(\@defenders, \@pos, $owner, $sector, 1);
+		}
+
+		if ($map_config{$object}{pirates}) {
+			my @pirates = split(",", $map_config{$object}{pirates});
+			my @pos = (); 
+
+			foreach my $pirate (@pirates) {
+				starmade_despawn_sector($pirate, 'unused', 0, $sector);
+			}
+
+			if ($map_config{$object}{pirate_pos}) {
+				@pos = split(",", $map_config{$object}{pirate_pos});
+			};
+			starmade_spawn_mobs_bulk(\@pirates, \@pos, $pirate_faction, $sector, 1);
+		}
+
+		if ($map_config{$object}{doodads}) {
+			my @doodads = split(",", $map_config{$object}{doodads});
+			my @pos = ();
+
+			foreach my $doodad (@doodads) {
+				starmade_despawn_sector($doodad, 'unused', 0, $sector);
+			}
+
+			if ($map_config{$object}{doodad_pos}) {
+				@pos = split(",", $map_config{$object}{doodad_pos});
+			};
+			starmade_spawn_mobs_bulk(\@doodads, \@pos, $doodad_faction, $sector, 0);
+		};
+	};
+	return 1;
+};
+
 # starmade_spawn_mobs_bulk
 # spawn a large number of different mobs
 # INPUT1: (array pointer) ship blueprints
@@ -212,5 +295,42 @@ sub starmade_despawn_mobs_bulk {
                 };
         };
 }
+
+
+## starmade_recenter_map
+# Changes the sectors in the map hash to have a center of the given coords
+# INPUT1: Map hash pointer
+# INPUT2: Sector to become center (space seperated string)
+sub starmade_recenter_map {
+	my %map = %{shift(@_)};
+	my $center = shift(@_);
+	my %new_map = %map;
+
+	for my $object (keys %map) {
+		if ($map{$object}{sector}) {
+			$new_map{$object}{sector} = starmade_location_add($map{$object}{sector}, $center);
+		}
+	}
+	return \%new_map;
+}
+
+## starmade_remap_map_factions
+# Changes the dummy faction numbers to the real numbers
+# INPUT1: Map hash pointer
+# INPUT2: hash pointer to mappings of dummy faction -> real faction
+sub starmade_remap_map_factions {
+	my %map = %{shift(@_)};
+	my %faction_map = %{shift(@_)};
+	my %new_map = %map;
+
+	for my $object (keys %map) {
+		if ($map{$object}{owner} && $faction_map{$map{$object}{owner}}) {
+			print "map: $map{$object}{owner} -> $faction_map{$map{$object}{owner}}\n";
+			$new_map{$object}{owner} = $faction_map{$map{$object}{owner}};
+		}
+	}
+	return \%new_map;
+}
+
 
 1;
