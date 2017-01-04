4adbcd78de4f1e673e25d409568835fbf67a605f
Jeryia <Jeryia@users.noreply.github.com>
2017-01-03 18:42:06 -0700

    added combat quests

--- src/stard/plugins.disabled/Civilization/lib/civ_lib.pm
+++ src/stard/plugins.disabled/Civilization/lib/civ_lib.pm
@@ -96,7 +96,7 @@ sub get_station_options {
 		starmade_pm($player, "The station appears to be missing!");
 		return \%blank_hash;
 	}
-	if (starmade_loc_distance($station_pos, $player_info{pos}) > 400) {
+	if (starmade_loc_distance($station_pos, $player_info{pos}) > 500) {
 		starmade_pm($player, "You are not close enough to the station to trade with it.");
 		return \%blank_hash;
 	}
@@ -118,9 +118,31 @@ sub quest_active_list {
 	close($fh);
 
 	foreach my $quest (@quests) {
-		$quest=~s/\s//g;
+		$quest=~s/^\s//g;
+		$quest=~s/\s$//g;
 	}
 	return \@quests;
+}
+
+## quest_active
+# Check if a given quest is in progress by the given player.
+# INPUT1: player
+# INPUT2: category of quest.
+# INPUT3: quest name
+# OUTPUT: 1 if active, 0 if not.
+sub quest_active {
+	my $player = shift(@_);
+	my $category = shift(@_);
+	my $quest = shift(@_);
+
+	my @quests = @{quest_active_list($player, $category)};
+
+	foreach my $active_quest (@quests) {
+		if ($quest eq $active_quest) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 ## quest_add
@@ -158,6 +180,7 @@ sub quest_add {
 	print $fh join("\n", @quests);
 	close($fh);
 	close($lock_fh);
+	quest_start_actions($player, $category, $quest);
 }
 
 ## quest_remove
@@ -175,7 +198,7 @@ sub quest_remove {
 	open(my $lock_fh, "<", $datafile);
 	flock($lock_fh, 2);
 
-	my @quests = quest_active_list($player, $category);
+	my @quests = @{quest_active_list($player, $category)};
 	for (my $i = 0; $i <= $#quests; $i++) {
 		my $active_quest = $quests[$i];
 		if ($active_quest eq $quest) {
@@ -184,7 +207,9 @@ sub quest_remove {
 	}
 
 	open(my $fh, ">", $datafile) or warn "failed to open '$datafile': $!\n";
-	print $fh join("\n", @quests);
+	if (@quests) {
+		print $fh join("\n", @quests);
+	}
 	close($fh);
 	close($lock_fh);
 }
@@ -201,14 +226,31 @@ sub quest_success {
 
 	my %quest_config = %{quest_info($category, $quest)};
 
-	my $message = "Quest '$quest' has been completed successfully!\n";
-	$message   .= "Your Rewards:\n";
+	my $message = '';
+
+	if ($quest_config{next_objective}) {
+		$message = "Objective Complete!\n";
+	}
+	else {
+		$message = "Quest '$quest' has been completed successfully!\n";
+	}
 	if ($quest_config{reward_credits}) {
+		$message   .= "Your Rewards:\n";
 		starmade_give_credits($player, $quest_config{reward_credits});
 		$message .= "  $quest_config{reward_credits} Credits\n";
 	}
 	starmade_pm($player, $message);
 	quest_remove($player, $category, $quest);
+
+	if ($quest_config{next_objective}) {
+		quest_add($player, $category, $quest_config{next_objective});
+		my %quest_info = %{quest_info($category, $quest_config{next_objective})};
+		starmade_pm($player, "New objective: $quest_info{objective_text}");
+	}
+	open(my $fh, ">>", "$PLAYER_DATA/$player/quest_c_$category");
+	flock($fh, 2);
+	print $fh "$quest\n";
+	close($fh);
 }
 
 ## quest_failure
@@ -251,4 +293,65 @@ sub quest_info {
 	}
 	return \%blank_hash;
 }
+
+sub quest_entity_name {
+	my $player = shift(@_);
+	my $name = shift(@_);
+
+	$name=~s/%player%/$player/g;
+	return $name;
+}
+
+sub quest_start_actions {
+	my $player = shift(@_);
+	my $category = shift(@_);
+	my $quest = shift(@_);
+
+	my %quest_info = %{quest_info($category, $quest)};
+
+	my %player_info = %{player_info($player)};
+
+	for (my $i = 1; $quest_info{"intro_message$i"}; $i++) {
+		if ($quest_info{"intro_delay$i"}) {
+			sleep $quest_info{"intro_delay$i"};
+		}
+		starmade_pm($player, $quest_info{"intro_message$i"});
+	}
+	
+	if (!$quest_info{sector} || $quest_info{sector} eq $player_info{sector} ) {
+		if (
+			$quest_info{template} eq 'destroy' &&
+			$quest_info{objective} && 
+			$quest_info{objective_bp} && 
+			$quest_info{objective_fac}
+		) {
+			starmade_spawn_entity_pos($quest_info{objective_bp}, $quest_info{objective}, $player_info{sector}, starmade_random_pos(), $quest_info{objective_fac}, 1);
+		}
+		if ($quest_info{enemies} && $quest_info{enemies_fac} && $quest_info{enemies_pos} ) {
+			my @enemies = split(',', $quest_info{enemies});
+			my @enemies_pos = split(',', $quest_info{enemies_pos});
+			starmade_spawn_mobs_bulk(\@enemies, \@enemies_pos, $quest_info{enemies_fac}, $player_info{sector}, 1);
+		}
+	}
+	if ($quest_info{escort} && $quest_info{escort_fac} && $quest_info{escort_bp}) {
+		starmade_spawn_entity_pos($quest_info{escort_bp}, $quest_info{escort}, $player_info{sector}, starmade_random_pos(), $quest_info{escort_fac}, 1);
+	}
+
+	# Must be last, as thie waits for the countdown to finish before continuing.
+	if ($quest_info{failure_countdown} && $quest_info{failure_countdown_name}) {
+		starmade_countdown($quest_info{failure_countdown}, $quest_info{failure_countdown_name});
+		sleep $quest_info{failure_countdown};
+		if (quest_active($player, $category, $quest)) {
+			quest_failure($player, $category, $quest);
+		}
+	}
+	if ($quest_info{success_countdown} && $quest_info{success_countdown_name}) {
+		starmade_countdown($quest_info{failure_countdown}, $quest_info{failure_countdown_name});
+		sleep $quest_info{failure_countdown};
+		if (quest_active($player, $category, $quest)) {
+			quest_success($player, $category, $quest);
+		}
+	}
+}
+
 1;
